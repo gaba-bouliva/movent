599dab6748f5c3f11571d1a1a5975eaf7844a676
gaba <gaba.bouliva@gmail.com>
2025-02-26 00:54:05 +0100

    add some helper function

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) handleHealthcheck(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	err := app.writeJSON(w, jsonPayload{"api": data}, http.StatusOK, nil)
+	if err != nil {
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
---
+++ cmd/api/helpers.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type jsonPayload map[string]any
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, payload jsonPayload, status int, headers http.Header) error {
+
+	json, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	json = append(json, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(json)
+
+	return nil
+}
