35b5a74d6820b634c31ff1464e795f34a9e45aa8
gaba <gaba.bouliva@gmail.com>
2025-03-07 04:46:17 +0100

    add readJSON helper func

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -34,5 +37,49 @@ func (app *application) writeJSON(w http.ResponseWriter, payload jsonPayload, st
 	w.WriteHeader(status)
 	w.Write(json)
 
+	return nil
+}
+
+// dst should be a pointer
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dest any) error {
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(&dest)
+	if err != nil {
+		var syntaxErr *json.SyntaxError
+		var unmarshalTypeErr *json.UnmarshalTypeError
+		var invalidUnmarshalErr *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
+		switch {
+		case errors.As(err, &syntaxErr):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxErr.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return fmt.Errorf("body contains badly-formed JSON")
+		case errors.As(err, &unmarshalTypeErr):
+			if unmarshalTypeErr.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeErr.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeErr.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unkown keys %s", fieldName)
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+		case errors.As(err, &invalidUnmarshalErr):
+			panic(err)
+		default:
+			return err
+		}
+
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err == nil {
+		return errors.New("body must only contain a single JSON value")
+	}
 	return nil
 }
